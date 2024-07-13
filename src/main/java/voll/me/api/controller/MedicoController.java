19995b5c4ec64b30292f919d9aca4e9350decc89
Jhonatan Pacheco <47837212+dokatec@users.noreply.github.com>
2024-07-13 01:04:45 -0300

    atualizando projeto

--- src/main/java/voll/me/api/controller/MedicoController.java
+++ src/main/java/voll/me/api/controller/MedicoController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import voll.me.api.medico.DadosCadastroMedico;
+import voll.me.api.medico.Medico;
+import voll.me.api.medico.MedicoRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,9 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
